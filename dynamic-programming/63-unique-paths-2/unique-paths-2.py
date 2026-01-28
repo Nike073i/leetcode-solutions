c375376ca4cb9921e89230ec24b5513505e1e33c
Nike073i <Nike073i@mail.ru>
2026-01-28 13:18:21 +0400

    add: 63-unique-paths-2 на python

---
+++ dynamic-programming/63-unique-paths-2/unique-paths-2.py
@@ -0,0 +1,16 @@
+# memory: O(n)
+# time: O(n * m)
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        m, n = len(obstacleGrid), len(obstacleGrid[0])
+        dp = [0] * n
+        dp[0] = 1
+        
+        for i in range(m):
+            for j in range(n):
+                if obstacleGrid[i][j]:
+                    dp[j] = 0
+                elif j > 0:
+                    dp[j] += dp[j-1]
+        
+        return dp[-1]
