fb77bbd648ce68b785a746301fc05f17fe001086
Nike073i <Nike073i@mail.ru>
2026-01-28 16:54:59 +0400

    change: упрощение решения 62 на python

--- dynamic-programming/62-unique-paths/unique-paths.py
+++ dynamic-programming/62-unique-paths/unique-paths.py
@@ -1,20 +1,11 @@
 # memory: O(n)
 # time: O(m * n)
 class Solution:
-    def createArray(self, length: int, default = 0) -> List[int]:
-        return [default] * length
-
     def uniquePaths(self, m: int, n: int) -> int:
-        if m == 1:
-            return 1
+        dp = [1] * n
         
-        prev = self.createArray(n, 1)
-        curr = self.createArray(n)
-
         for i in range(1, m):
-            curr[0] = 1
             for j in range(1, n):
-                curr[j] = prev[j] + curr[j - 1]
-            prev, curr = curr, prev
+                dp[j] += dp[j - 1]
 
-        return prev[-1]
+        return dp[-1]
