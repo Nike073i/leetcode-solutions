5e51b52f61aa9c3ac5fb894e3797541074e52550
Nike073i <Nike073i@mail.ru>
2026-02-16 13:19:48 +0400

    add: 190-reverse-bits на python

---
+++ bit/190-reverse-bits/reverse-bits-in-loop.py
@@ -0,0 +1,10 @@
+# time: O(1)
+# memory: O(1)
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        result = 0
+        for i in range(0, 32):
+            result = (result << 1) | (n & 1)
+            n >>= 1
+        
+        return result
